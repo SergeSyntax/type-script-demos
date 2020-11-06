df97202dc15f64f987ffbda45e85d60dcaeb280d
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2020-11-06 09:31:24 +0200

    created sort alg demo in type script

---
+++ type-script/sort/src/index.ts
@@ -0,0 +1,24 @@
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
+import { NumbersCollection } from './NumbersCollections';
+import { Sorter } from './Sorter';
+
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+numbersCollection.sort();
+
+console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xaayb');
+charactersCollection.sort();
+console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.sort();
+
+console.log(linkedList.print());
+;
