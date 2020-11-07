74cc074a10752ac2b44f21c3e5fcf1c4ebcc229d
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2020-11-07 22:08:50 +0200

    created ts demo using enums, tuples, composition and inheritance

---
+++ type-script/stats/src/untils.ts
@@ -0,0 +1,5 @@
+export const dataStringToDate = (dateString: string) => {
+  const dateParts = dateString.split('/').map((value: string): number => parseInt(value));
+
+  return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+};
