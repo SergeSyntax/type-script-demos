48b4d3c6042e36a35a690915ccfd703fe31d6d4a
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2020-12-24 03:51:59 +0200

    change to typescript demos

--- angular/mathform/src/app/math-validators.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MathValidators } from './math-validators';
-
-describe('MathValidators', () => {
-  it('should create an instance', () => {
-    expect(new MathValidators()).toBeTruthy();
-  });
-});
